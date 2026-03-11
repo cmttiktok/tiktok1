bc152caa72cd882a7556be393f01f2c1947e2104
cmttiktok <phamanhtu535@gmail.com>
2026-03-11 21:51:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,28 +5,21 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const { WebcastPushConnection } = require('tiktok-live-connector');
 
-// CHỈNH SỬA: Đảm bảo Server đọc được file index.html dù bạn để ở đâu
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')); });
 
 io.on('connection', (socket) => {
     let tiktok;
     socket.on('set-username', (user) => {
         if (tiktok) tiktok.disconnect();
         tiktok = new WebcastPushConnection(user);
-        tiktok.connect().then(s => socket.emit('status', {ok:true}))
-              .catch(e => socket.emit('status', {ok:false}));
+        tiktok.connect().then(s => socket.emit('status', {ok:true, profile: s.ownerInfo?.avatarThumb})).catch(e => socket.emit('status', {ok:false}));
 
         tiktok.on('chat', (d) => {
-            io.emit('audio-data', {
-                user: d.nickname,
-                comment: d.comment,
-                profile: d.profilePictureUrl // Lấy avatar
-            });
+            io.emit('audio-data', { user: d.nickname, comment: d.comment, profile: d.profilePictureUrl, type: 'chat' });
+        });
+        tiktok.on('gift', (d) => {
+            io.emit('audio-data', { user: d.nickname, comment: `tặng ${d.giftName}`, profile: d.profilePictureUrl, type: 'gift' });
         });
     });
 });
-
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log('Server is running!'));
+http.listen(process.env.PORT || 3000);
